008f6fa91d070df5e1848d46ff61e1d450be51be
Joseph Congdon <jocongdon@yahoo.com>
2022-01-27 15:01:34 -0600

    will this work

--- com/aa/act/interview/org/Organization.java
+++ com/aa/act/interview/org/Organization.java
@@ -1,5 +1,6 @@
 package com.aa.act.interview.org;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public abstract class Organization {
@@ -18,9 +19,18 @@ public abstract class Organization {
 	 * @param person
 	 * @param title
 	 * @return the newly filled position or empty if no position has that title
+	 * //Joseph Congdon notes
+	 * The Position of COO is not filled by anyone inside the MyOrganization abstract class
+	 * If the parameter 'title' listed in the Object "Name" matches the Object of "Position" parameter of 'title' return a string that confirms that the position.title has been filled by the Name.first Name.last.
+	 *
 	 */
 	public Optional<Position> hire(Name person, String title) {
 		//your code here
+		Arrays.stream(new Position("").getClass().getMethods()).toArray();
+		if (title.equals(createOrganization().getEmployee())){
+
+		}
+
 		return Optional.empty();
 	}
 
---
+++ out/production/Java_coding_test/Readme.md
@@ -0,0 +1,3 @@
+Instructions – HW
+
+Provide an implementation of the hire() method in the Organization class.  You can change anything except what is in MyOrganization.
